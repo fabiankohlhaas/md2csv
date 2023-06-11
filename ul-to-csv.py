aa4d951513ee5a9da7c0804e4aa929696a63bf2f
Fabian Kohlhaas <fabian@kohlhaas.email>
2023-06-11 13:05:40 +0200

    Add succes message

--- ul-to-csv.py
+++ ul-to-csv.py
@@ -15,8 +15,9 @@ def markdown_to_csv(markdown_filename, csv_filename):
     with open(csv_filename, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter='|')
         writer.writerows(questions_answers)
+        print("CSV file created successfully!")
 
 if __name__ == "__main__":
-    markdown_filename = "input.md" #input("Please enter the path of the Markdown file: ")
-    csv_filename = "import-to-anki.csv" #input("Please enter the path of the CSV file to be created: ")
+    markdown_filename = "input/input.md" #input("Please enter the path of the Markdown file: ")
+    csv_filename = "output/import-to-anki.csv" #input("Please enter the path of the CSV file to be created: ")
     markdown_to_csv(markdown_filename, csv_filename)
